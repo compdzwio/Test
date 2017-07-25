56edf6b6e05513659c8bf3206e4c3683fe27c0b6
czom <web_czom@yeah.net>
2017-07-25 15:05:53 +0800

    Create gendlldef.bat

---
+++ gendlldef.bat
@@ -0,0 +1,24 @@
+@echo off
+if exist "%1.def" (
+  del %1.def
+)
+set /A c = 0
+echo EXPORTS >> %1.def
+if exist c:\windows\system32\%1.dll (
+  for /f "tokens=4,3,1" %%i in ('dumpbin /exports c:\windows\system32\%1.dll') do (
+    set /A c=!c!+1
+    if !c! gtr 12 (
+      if (forwarded==%%k (
+        echo %%j @ %%i >> %1.def
+      ) else (
+        if not "%%k"=="" (
+          echo %%k @ %%i >> %1.def
+        )
+      )
+    )
+  )
+  echo complete
+) else (
+  echo dll exists!
+)
+@echo on
